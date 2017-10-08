8b7eeee0a08ef2edbdba5d0f91b6d77b8a2580bc
Peony Gerochi <itsduc@gmail.com>
2017-10-08 09:04:51 -0400

    - fixed javascript logic - styling

--- app/index.js
+++ app/index.js
@@ -1,44 +1,79 @@
-'use strict';
+import clock from "clock";
+import document from "document";
+import { HeartRateSensor } from "heart-rate";
+import { today, goals } from "user-activity"
+import * as util from "../common/utils";
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+// Update the clock every minute
+clock.granularity = "seconds";
 
-var clock = _interopDefault(require('clock'));
-var document = _interopDefault(require('document'));
-var heartRate = require('heart-rate');
-var userActivity = require('user-activity');
+// Get a handle on the <text> element
+let userMonth = document.getElementById("userMonth");
+let userTimeSecond = document.getElementById("userTimeSecond");
+let userTime = document.getElementById("userTime");
+let userCalories = document.getElementById("userCalories");
+let userCaloriesText = document.getElementById("userCaloriesText");
+let userSteps = document.getElementById("userSteps");
+let amPM = document.getElementById("amPM");
 
-function zeroPad(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
-}
+let smallHeart = document.getElementById("smallHeart");
+let heartRateInformation = document.getElementById("heartRateInformation");
 
-clock.granularity = "minutes";
-var userMonth = document.getElementById("userMonth");
-var userTime = document.getElementById("userTime");
-var userCalories = document.getElementById("userCalories");
-var smallHeart = document.getElementById("smallHeart");
-var heartRateInformation = document.getElementById("heartRateInformation");
-var monthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+//Month Array 
+let monthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+  "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
 ];
-var hrm = new heartRate.HeartRateSensor();
+
+//Day Array 
+let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", 
+"Thursday", "Friday", "Saturday"];
+
+// Create a new instance of the HeartRateSensor object
+let hrm = new HeartRateSensor();
+heartRateInformation.innerText = "--";  
+
+// Update the <text> element with the current time
 function updateClock() {
-    var dayInfo = new Date();
-    var hours = dayInfo.getHours();
-    var mins = zeroPad(dayInfo.getMinutes());
-    var day = dayInfo.getDay() + 1;
-    var month = dayInfo.getMonth();
-    userMonth.innerText = monthNames[month] + " " + day;
-    userTime.innerText = hours + ":" + mins;
-    userCalories.innerText = userActivity.today.adjusted.steps;
+  let dayInfo = new Date();
+  let hour = dayInfo.getHours() > 12 ? dayInfo.getHours() - 12 : dayInfo.getHours();
+  let amPmInfo = dayInfo.getHours() >= 12 ? "PM" : "AM";
+  let mins = util.zeroPad(dayInfo.getMinutes());
+  let sec = util.zeroPad(dayInfo.getSeconds());
+
+  let day = dayInfo.getDay();
+  let month = dayInfo.getMonth();
+  let dayOfMonth = dayInfo.getDate();
+  userMonth.innerText = `${monthNames[month]} ${dayOfMonth}`;
+  userCaloriesText.innerText = dayNames[day];
+  userSteps.innerText = today.adjusted.steps;
+  
+  hour = hour < 10 ? "0" + hour : hour;
+  userTime.innerText = `${hour}:${mins}`;
+  userTimeSecond.innerText = `${sec}`;
+  amPM.innerText = amPmInfo;
 }
-var hrm = new heartRate.HeartRateSensor({ frequency: 10, batch: 60 });
-hrm.onreading = function () {
-    console.log("Current heart rate: " + hrm.heartRate);
-    heartRateInformation.innerText = hrm.heartRate;
-};
+
+//Declare heart rate
+let hrm = new HeartRateSensor({ frequency: 10, batch: 60 });
+
+// Declare a even handler that will be called every time a new HR value is received.
+hrm.onreading = function() {
+  // Peek the current sensor values
+   // Loop through this batch of readings[]
+  hrm.readings.forEach(function (reading) {
+    //console.log("Current heart rate: " + reading.heartRate);
+    heartRateInformation.innerText = reading.heartRate;  
+  });
+ 
+}
+
+// Update the clock every tick event
+clock.ontick = () => updateClock();
+
+// Begin monitoring the sensor
 hrm.start();
-clock.ontick = function () { return updateClock(); };
+
+// Don't start with a blank screen
 updateClock();
+
+
